0a707dea9b7d96701fd68380c89b5ed0edf7be6f
Igor Pakshin <38372496+mephistofeel@users.noreply.github.com>
2020-04-22 14:51:19 +0300

    Upd
    Signed-off-by: Igor Pakshin <38372496+mephistofeel@users.noreply.github.com>

---
+++ source/js/main.js
@@ -0,0 +1,18 @@
+var acc = document.getElementsByClassName("cv-card__item-external");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Переключение между добавлением и удалением "active" class,
+    выделить кнопку, управляющую панелью */
+    this.classList.toggle("active");
+
+    /* Переключение между скрытием и отображением активной панели */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
